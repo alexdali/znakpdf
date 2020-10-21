02ec3d1b60102770cd59b9edd0f02776824888da
damu4@yandex.ru <damu4@yandex.ru>
2020-10-21 15:49:30 +0300

    feat: make parse in temporary files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from io import BytesIO
-from os import remove
-from random import randint
-from shutil import copyfileobj
 
 import fitz
 from PIL import Image
@@ -11,83 +8,53 @@ from pylibdmtx.pylibdmtx import decode
 
 app = FastAPI()
 
+zoom = 2
+w = 148
+h = 148
+xys = [(444, 72, 444 + w, 72 + h),
+       (1060, 72, 1060 + w, 72 + h),
+       (444, 416, 444 + w, 416 + h),
+       (1060, 416, 1060 + w, 416 + h),
+       (444, 760, 444 + w, 760 + h),
+       (1060, 760, 1060 + w, 760 + h)]
 
-@app.post("/decodefile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    zoom = 2
-    w = 148
-    h = 148
-    temp_file = f'/tmp/temp{randint(0, 100)}.pdf'
-    xys = [(444, 72, 592, 220),
-           (1060, 72, 1060 + w, 72 + h),
-           (444, 416, 592, 416 + h),
-           (1060, 416, 1060 + w, 416 + h),
-           (444, 760, 592, 760 + h),
-           (1060, 760, 1060 + w, 760 + h)]
+
+async def parse_pdf(file):
     codes = []
 
     try:
-        with open(temp_file, "wb") as buffer:
-            copyfileobj(file.file, buffer)
-            mat = fitz.Matrix(zoom, zoom)
-            doc = fitz.open(temp_file)
+        file_bytes = await file.read()
+        mat = fitz.Matrix(zoom, zoom)
+        doc = fitz.open("pdf", file_bytes)
 
-            for i, page in enumerate(doc.pages()):
-                pix = page.getPixmap(matrix=mat, alpha=True)
-                bytes_image = pix.getImageData("png")
-                image = Image.open(BytesIO(bytes_image))
+        for i, page in enumerate(doc.pages()):
+            pix = page.getPixmap(matrix=mat, alpha=True)
+            bytes_image = pix.getImageData("png")
+            image = Image.open(BytesIO(bytes_image))
 
-                for xy in xys:
-                    try:
-                        cropped = image.crop(xy)
-                        t = decode(cropped.convert('L'))[0].data.decode('utf-8')
-                        codes.append(t)
-                    except IndexError:
-                        pass
-
-        remove(temp_file)
-    except:
-        return {"Error"}
+            for xy in xys:
+                try:
+                    cropped = image.crop(xy)
+                    t = decode(cropped.convert('L'))[0].data.decode('utf-8')
+                    codes.append(t)
+                except IndexError:
+                    pass
+        doc.close()
+    except Exception as e:
+        print(e)
+        return "Error"
 
     return {"codes": codes,
-            "n:": len(codes)}
+            "n": len(codes)}
+
+
+@app.post("/decodefile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    codes = await parse_pdf(file)
+    return codes
 
 
 @app.post("/decodefiletotext/", response_class=PlainTextResponse)
 async def create_upload_file(file: UploadFile = File(...)):
-    zoom = 2
-    w = 148
-    h = 148
-    temp_file = f'/tmp/temp{randint(0, 100)}.pdf'
-    xys = [(444, 72, 592, 220),
-           (1060, 72, 1060 + w, 72 + h),
-           (444, 416, 592, 416 + h),
-           (1060, 416, 1060 + w, 416 + h),
-           (444, 760, 592, 760 + h),
-           (1060, 760, 1060 + w, 760 + h)]
-    codes = []
-
-    try:
-        with open(temp_file, "wb") as buffer:
-            copyfileobj(file.file, buffer)
-            mat = fitz.Matrix(zoom, zoom)
-            doc = fitz.open(temp_file)
-
-            for i, page in enumerate(doc.pages()):
-                pix = page.getPixmap(matrix=mat, alpha=True)
-                bytes_image = pix.getImageData("png")
-                image = Image.open(BytesIO(bytes_image))
-
-                for xy in xys:
-                    try:
-                        cropped = image.crop(xy)
-                        t = decode(cropped.convert('L'))[0].data.decode('utf-8')
-                        codes.append(t)
-                    except IndexError:
-                        pass
-
-        remove(temp_file)
-    except:
-        return {"Error"}
-
-    return '\n'.join(codes) + '\n' + str(len(codes))
+    codes = await parse_pdf(file)
+    return '\n'.join(codes['codes']) + '\n' + str(codes['n'])
