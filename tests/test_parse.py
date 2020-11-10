d5c55ef8f8de675e5ecb13ee5b6e202768d38bb8
damu4@yandex.ru <damu4@yandex.ru>
2020-11-10 11:17:35 +0300

    feat: added test

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,10 +1,22 @@
-from app.main import parse_pdf
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
 
 
-def test_parse80():
-    try:
-        with open('80.pdf', 'rb') as file:
-            codes = await parse_pdf(file)
-    except:
-        pass
-    assert True
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
+
+
+def test_parse80(test_app):
+    with open('80.pdf', 'rb') as file:
+        response = test_app.post(
+            "/decodefile/",
+            files={"file": ("filename", file)})
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['n'] == 80
+    assert data['codes'][-1] == \
+           "010460373162109221ikI>HEJLtE:tD\u001d918069\u001d92gBRfERwFH6vF/haAG8ADNMGaTdry3V0j1rfD6OwPJlvK5ftyRbFUnJFdOO7FAZv7FVtwYqkGwLyJ2cwHAjDzRA=="
