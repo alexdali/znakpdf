c48585437bc96d5783e92bd0bc555b4f64dfbb05
damu4@yandex.ru <damu4@yandex.ru>
2020-11-10 10:20:53 +0300

    fix: travis

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -4,7 +4,7 @@ from app.main import parse_pdf
 def test_parse80():
     try:
         with open('80.pdf', 'rb') as file:
-            parse_pdf(file)
+            codes = await parse_pdf(file)
     except:
         pass
     assert True
